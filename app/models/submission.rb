857f3bf53867e659454be7c0475bf67b36af9027
Brian Nickerson <nickerson.brian@gmail.com>
2017-01-04 16:02:50 -0500

    Add validations to Submissions fields. Special regex validation for email. Define 'sum_of_attributes' to be used later for checking sum of attributes does not exceed 50.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,26 @@
 class Submission < ApplicationRecord
+  validates :full_name,
+            presence: true,
+            length: { in: 3..60 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            length: { in: 6..50 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :project_repo,
+            presence: true
+  validates :project_url,
+            presence: true
+  validates :oop_best_practices, :modular_dev, :workflow_understanding, :testing, :database_knowledge, :debugging, :problem_solving, :javascript, :html, :css, :teamwork, :self_motivation, :communication_skils, :energy_level, :intelligence,
+            presence: true,
+            length: { in: 1..2 },
+            numericality: { only_integer: true }
+
+
+  private
+
+    def sum_of_attributes
+      oop_best_practices + modular_dev + workflow_understanding + testing + database_knowledge + debugging + problem_solving + javascript + html + css + teamwork + self_motivation + communication_skils + energy_level + intelligence
+    end
 end
