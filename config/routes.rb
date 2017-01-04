600d622e462547a22c07e0529dcf8f538e66b74f
Brian Nickerson <nickerson.brian@gmail.com>
2017-01-04 16:49:26 -0500

    Remove links to Index page. Remove unused Controller actions. Add routing alias.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'submissions#new'
+  get '/', to: 'submissions#new', as: 'submissions'
   post '/', to: 'submissions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
