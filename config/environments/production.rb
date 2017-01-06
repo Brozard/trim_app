33e63a192a9a6feb2cff1a15306ff59e7b6d7285
Brian Nickerson <nickerson.brian@gmail.com>
2017-01-06 16:26:59 -0500

    Update production config environment with SendGrid info.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'aqueous-plains-46447.herokuapp.com',
-    user_name:            'omnischedulerapp',
-    password:             ENV['OMNI_SCHEDULE_GMAIL_PASSWORD'],
+    domain:               'heroku.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
